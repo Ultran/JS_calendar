a9ef991e53a0766d482edc76c46d84a4c5bfa335
Slawek <61154596+Ultran@users.noreply.github.com>
2020-12-20 23:12:55 +0100

    change name of component clasa.js -> script.js

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-const dayOne = document.querySelector(".day-one");
-const dayTwo = document.querySelector(".day-two");
-const dayThree = document.querySelector(".day-three");
-const dayFour = document.querySelector(".day-four");
-const dayFive = document.querySelector(".day-five");
-const daySix = document.querySelector(".day-six");
-const daySeven = document.querySelector(".day-seven");
+class Calendar {
+  constructor(startedTimestamp) {
+    if (isNaN(startedTimestamp.getTime())) {
+      throw new Error("invalid date");
+    }
 
-const todayDate = new Date();
-
-let newDate = 0;
-
-const addWeek = function () {
-  newDate.setDate(newDate.getDate() + 1);
-  setNewDate(newDate);
-  console.log(newDate);
-};
-
-const subtractWeek = function () {
-  newDate.setDate(newDate.getDate() - 13);
-  setNewDate(newDate);
-  console.log(newDate);
-};
-
-function setNewDate(dateInput) {
-  function addDay(dateInput) {
-    newDate = dateInput;
-    newDate.setDate(newDate.getDate() + 1);
-    return newDate;
+    this.now = new Date(startedTimestamp);
   }
-  dayOne.textContent = dateInput;
-  dayTwo.textContent = addDay(dateInput);
-  dayThree.textContent = addDay(dateInput);
-  dayFour.textContent = addDay(dateInput);
-  dayFive.textContent = addDay(dateInput);
-  daySix.textContent = addDay(dateInput);
-  daySeven.textContent = addDay(dateInput);
+
+  createButtons() {
+    const calcContainer = document.querySelector(".calendar");
+    const buttonPrevious = document.createElement("button");
+    const buttonNext = document.createElement("button");
+    buttonPrevious.textContent = "last week";
+    buttonNext.textContent = "next week";
+    buttonPrevious.classList.add("button1");
+    buttonNext.classList.add("button2");
+    calcContainer.appendChild(buttonPrevious);
+    calcContainer.appendChild(buttonNext);
+    buttonPrevious.addEventListener("click", this.lastweek);
+    buttonNext.addEventListener("click", this.nextWeek);
+  }
+
+  createCalendar() {
+    const calcContainer = document.querySelector(".calendar");
+    let addDate = this.now;
+
+    for (let i = 0; i < 7; i++) {
+      const div = document.createElement("div");
+      div.classList.add("calendar__day");
+      div.textContent = addDate;
+      calcContainer.appendChild(div);
+      addDate.setDate(addDate.getDate() + 1);
+    }
+    this.createButtons();
+  }
+
+  nextWeek() {
+    for (let i = 1; i < 8; i++) {
+      document.querySelector(`.calendar__day:nth-child(${i})`).textContent =
+        calendar.now;
+      calendar.now.setDate(calendar.now.getDate() + 1);
+    }
+  }
+  lastweek() {
+    calendar.now.setDate(calendar.now.getDate() - 14);
+    for (let i = 1; i < 8; i++) {
+      document.querySelector(`.calendar__day:nth-child(${i})`).textContent =
+        calendar.now;
+      calendar.now.setDate(calendar.now.getDate() + 1);
+    }
+  }
 }
 
-setNewDate(todayDate);
-
-document.querySelector(".button-right").addEventListener("click", addWeek);
-document.querySelector(".button-left").addEventListener("click", subtractWeek);
+const startDate = new Date();
+const calendar = new Calendar(startDate);
+calendar.createCalendar();
